8fb6fdb669b21829d6817ce185ba49b25f7ce99f
AnnaFesenko7 <annafecenko7@gmail.com>
2023-07-13 20:29:37 +0300

    field array

--- src/components/FormikComponent/FormikComponent.js
+++ src/components/FormikComponent/FormikComponent.js
@@ -1,4 +1,4 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Field, Form, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
 import { TextError } from './TextError';
 
@@ -14,6 +14,8 @@ const initialValues = {
     facebook: '',
     twitter: '',
   },
+  phoneNumber: ['', ''],
+  phNumbers: [''],
 };
 
 const onSubmit = values => {
@@ -42,11 +44,13 @@ export const FormikComponent = () => {
             <Field name="name" type="text" id="name" />
             <ErrorMessage name="name" component={TextError} />
           </div>
+
           <div className="control">
             <label htmlFor="email">Email</label>
             <Field name="email" type="email" id="email" />
             <ErrorMessage name="email" component={TextError} />
           </div>
+
           <div className="control">
             <label htmlFor="channel">Channel</label>
             <Field
@@ -57,6 +61,7 @@ export const FormikComponent = () => {
             />
             <ErrorMessage name="channel" component={TextError} />
           </div>
+
           <div className="control">
             <label htmlFor="comments">Comments</label>
             <Field name="comments" id="comments" as="textarea" placeholder="" />
@@ -64,12 +69,13 @@ export const FormikComponent = () => {
               {errorMsg => <div className="error">{errorMsg}</div>}
             </ErrorMessage>
           </div>
+
           <div className="control">
             <label htmlFor="address">Address</label>
             <Field name="address">
               {props => {
                 const { field, meta } = props;
-                console.log('props', props);
+                // console.log('props', props);
                 return (
                   <div>
                     <input id="address" {...field} type="text" />
@@ -90,6 +96,66 @@ export const FormikComponent = () => {
             <label htmlFor="twitter">Twitter</label>
             <Field name="social.twitter" type="text" id="twitter" />
             <ErrorMessage name="social.twitter" component={TextError} />
+          </div>
+          <div className="control">
+            <label htmlFor="primaryPhoneNumber">Primary phone number</label>
+            <Field name="phoneNumber[0]" type="text" id="primaryPhoneNumber" />
+            <ErrorMessage name="phoneNumber[0]" component={TextError} />
+          </div>
+          <div className="control">
+            <label htmlFor="secondaryPhoneNumber">Secondary phone number</label>
+            <Field
+              name="phoneNumber[1]"
+              type="text"
+              id="secondaryPhoneNumber"
+            />
+            <ErrorMessage name="phoneNumber[1]" component={TextError} />
+          </div>
+          <div className="control">
+            <label>List of phone numbers</label>
+            <FieldArray name="phNumbers">
+              {
+                fieldArrProps => {
+                  const { push, remove, form } = fieldArrProps;
+                  const { values } = form;
+                  const { phNumbers } = values;
+                  console.log(
+                    '🚀 ~ file: FormikComponent.js:118 ~ FormikComponent ~ phNumbers:',
+                    phNumbers
+                  );
+                  return (
+                    <div>
+                      {phNumbers.map((phNumber, index) => (
+                        <div key={index}>
+                          <Field name={`phNumbers[${index}]`} />
+                          {index > 0 && (
+                            <button type="button" onClick={() => remove(index)}>
+                              -
+                            </button>
+                          )}
+
+                          <button type="button" onClick={() => push('')}>
+                            +
+                          </button>
+                        </div>
+                      ))}
+                    </div>
+                  );
+                }
+
+                //       {
+                //         phNumbers.map((phNumber, index) => (
+                //           <div key={index}>
+                //
+                //         </div>))
+
+                //       }
+                //return (
+                //
+                //   );
+              }
+            </FieldArray>
+            <ErrorMessage name="phNumbers" component={TextError} />
           </div>
           <button type="submit">Submit</button>
         </Form>
